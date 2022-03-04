62e9aa2868f92901ac473f96ed8edadd48092c6a
Herdi Apriansyah <laplacedemon9@gmail.com>
2022-03-04 11:07:38 +0700

    membuat navbar dan template untuk artikel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
     content: ["./public/**/*{html, js}"],
     theme: {
         extend: {},
+        container: {
+            center: true,
+        }
     },
     plugins: [],
 }
